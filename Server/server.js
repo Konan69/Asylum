b80ba7cad77ab93cc1b534b570746b4a794c84e8
Konan <kixeyems0@gmail.com>
2024-04-11 19:29:50 +0100

    added db config and db schema

--- Server/server.js
+++ Server/server.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const app = express()
 const cors = require('cors')
+const mongoose = require('mongoose')
+require('dotenv').config()
 
 app.use(cors())
 app.options('*', cors)
@@ -9,5 +11,11 @@ app.use(express.json())
 app.get("/api", (req, res) => {
   res.json({"users": ["UserOne", "UserTwo", "UserThree"]})
 })
+
+
+// db connect 
+mongoose.connect('mongodb+srv://kixeyems0:wAjlc6D4pesyPEAV@cluster0.kw5stww.mongodb.net/cluster0?retryWrites=true&w=majority')
+.then(()=> console.log('connected to db'))
+.catch((err)=> console.log(err))
 
 app.listen(8080, () => { console.log("listening to server")})
