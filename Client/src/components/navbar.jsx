88989958e356b65f596fd5cc2a76f9017fd411ea
Konan <kixeyems0@gmail.com>
2024-04-21 14:09:08 +0100

    mm

--- Client/src/components/navbar.jsx
+++ Client/src/components/navbar.jsx
@@ -6,7 +6,7 @@ import '../Styles/navbar.css'
 function Navbar({ handleNavButtonClick }) {
   return (
     <header className="header">
-      <div className="basebound"> <Link to="/">BASEBOUND</Link> </div>
+      <div className="basebound"> <Link to="/">BASEBOUND </Link> </div>
       <div className="navbar">
         <button className="nav-button" onClick={() => handleNavButtonClick(1)}>NFT</button>
         <button className="nav-button" onClick={() => handleNavButtonClick(2)}>The Bound</button>
