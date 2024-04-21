42c91309271959634338921b45df54b12b096ed6
Konan <kixeyems0@gmail.com>
2024-04-21 15:55:21 +0100

    .

--- Client/vite.config.js
+++ Client/vite.config.js
@@ -3,14 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  server: {
-    proxy: {
-      // Proxy configuration
-      '/api': {
-        target: 'https://asylum-backend.vercel.app',
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, '')}}}
-        ,
   plugins: [react()],
   
 })
