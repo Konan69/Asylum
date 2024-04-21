8afbade033456de8d15269b642574a176a79acc3
Konan <kixeyems0@gmail.com>
2024-04-21 14:39:04 +0100

    .

---
+++ Client/src/components/Footer.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import { FaXTwitter } from "react-icons/fa6";
+import '../Styles/footer.css'
+
+
+function Footer() {
+   
+  return (
+    <footer class = 'footer'>
+    <div className="socials"> 
+    <FaXTwitter />
+    </div>
+    </footer> 
+  )
+}
+
+export default Footer;
---
+++ Server/Models/user.js
@@ -0,0 +1,51 @@
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema({
+  wallet: {
+    type: String,
+    required: true,
+    unique: true,
+    length: 42
+  },
+  referrals: {
+    type: Number,
+    required: true,
+    default:0
+  },
+  points: {
+    type: Number,
+    required:true,
+    default: 0
+  },
+  TwitterFollowed : {
+    type: Boolean,
+    required: true,
+    default: false,
+  },
+  PostLiked : {
+    type: Boolean,
+    required: true,
+    default: false,
+  },
+  TweetPosted: {
+    type: Boolean,
+    required: true,
+    default: false,
+  },
+  Whitelist: {
+    type:  Boolean,
+    required: true,
+    default: false
+  }
+})
+
+userSchema.virtual('id').get(function () {
+  return this._id.toHexString();
+});
+
+userSchema.set('toJSON', {
+  virtuals: true,
+});
+
+exports.User = mongoose.model('User', userSchema)
+exports.userSchema = userSchema
